44e626c5da2e1a911dadedcd1fa709dbf0e05ef6
felixschulz385 <felix@zoelch-schulz.de>
2023-01-25 17:13:55 +0000

    Added exception handling

--- NBER_scraping_ext.py
+++ NBER_scraping_ext.py
@@ -20,34 +20,42 @@ def main():
         time.sleep(.5)
         
         # get the page and turn into parsable soup
-        soup = BeautifulSoup(requests.get("https://www.nber.org/" + link).content, 'html.parser')
+        try:
+            soup = BeautifulSoup(requests.get("https://www.nber.org/" + link).content, 'html.parser')
+        except:
+            continue
         
         # initialize output dictionary
         out = {}
         
-        # retrieve abstract
-        out["abstract"] = soup.find("div", {"class": "page-header__intro-inner"}).get_text()
+        try:
         
-        # retrieve acknowledgements and disclosures
-        out["a_d"] = soup.find("div", {"id": "accordion-body-guid1"}).p.get_text()
-        
-        # retrieve auxiliary links
-        out["links"] = [x["href"] for x in soup.find_all("a", {"class": "link link--arrow"})]
-        
-        # retrieve DOI
-        out["doi"] = re.sub("\nDOI |\n  ", "", soup.find_all("div", {"class": "page-header__citation-item"})[1].get_text())
-        
-        # retrieve associated topics and programs
-        tmp = soup.find_all("div", {"class": "info-grid__item-body"})
-        out["topics"] = tmp[0].find_all(text = True)
-        out["programs"] = tmp[1].find_all(text = True)
+            # retrieve abstract
+            out["abstract"] = soup.find("div", {"class": "page-header__intro-inner"}).get_text()
+            
+            # retrieve acknowledgements and disclosures
+            out["a_d"] = soup.find("div", {"id": "accordion-body-guid1"}).p.get_text()
+            
+            # retrieve auxiliary links
+            out["links"] = [x["href"] for x in soup.find_all("a", {"class": "link link--arrow"})]
+            
+            # retrieve DOI
+            out["doi"] = re.sub("\nDOI |\n  ", "", soup.find_all("div", {"class": "page-header__citation-item"})[1].get_text())
+            
+            # retrieve associated topics and programs
+            tmp = soup.find_all("div", {"class": "info-grid__item-body"})
+            out["topics"] = tmp[0].find_all(text = True)
+            out["programs"] = tmp[1].find_all(text = True)
+            
+        except:
+            pass
         
         # write to list
         data[index] = out
             
         # save checkpoint
         if index % 10 == 0:
-            with open("nber_data_detailed.json", "w") as f:
+            with open("/home/ubuntu/ext_drive/HiWi/mlecon_scraping/ML-in-Business-Economics/nber_data_detailed.json", "w") as f:
                 json.dump(data, f)
 
 
